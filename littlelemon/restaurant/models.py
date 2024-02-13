c1c5800ae5a53ef3f54a688fd10e12f6b411fc6d
Juan Sebastián Arias Valero <126701235+JSA-V@users.noreply.github.com>
2024-02-13 10:05:44 -0500

    Set up the menu API

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -17,3 +17,8 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.title
+    
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
